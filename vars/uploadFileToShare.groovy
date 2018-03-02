1633adbaf47ee270a75ded63a23caeca75014ded
Michael Musenbrock <michael.musenbrock@gmail.com>
2018-03-02 13:59:19 +0100

    uploadFileToShare: Use ssh-agent instead of referencing the key file directly

--- vars/uploadFileToShare.groovy
+++ vars/uploadFileToShare.groovy
@@ -5,6 +5,9 @@ def call(def localfile, def remotefile)
 	echo "Upload file '${localfile}' to share as '${remotefile}'"
 	sh "mv -f \"${localfile}\" \"${remotefile}\""
 	sh "sha256sum \"${remotefile}\" > \"${remotefile}.sha256\""
-	sh "echo \"put ${remotefile}\"        | sftp -i ~/.ssh/jenkins-file-upload -o StrictHostKeychecking=no -b- file-downloads@files.catrob.at:www"
-	sh "echo \"put ${remotefile}.sha256\" | sftp -i ~/.ssh/jenkins-file-upload -o StrictHostKeychecking=no -b- file-downloads@files.catrob.at:www"
+
+	sshagent (credentials: ['files-catrob-at-upload']) {
+		sh "echo \"put ${remotefile}\"        | sftp -o StrictHostKeychecking=no -b- file-downloads@files.catrob.at:www"
+		sh "echo \"put ${remotefile}.sha256\" | sftp -o StrictHostKeychecking=no -b- file-downloads@files.catrob.at:www"
+	}
 }
