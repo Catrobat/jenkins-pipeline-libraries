661f98cdff6d97a0368455ac3cec1b9c26fa40e3
Michael Musenbrock <michael.musenbrock@gmail.com>
2018-06-20 10:27:17 +0200

    JENKINS-209: Implement uploadFileToWeb

---
+++ vars/uploadFileToWeb.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+def call(def localfile, def upload)
+{
+	echo "Uploading file '${localfile}' to WEB"
+	// +x, otherwise we would spoil the upload token
+	//sh "set +x; curl -X POST -k -F upload=@${localfile} '${upload}'"
+	sh "set +x; echo TO UPLOAD ${localfile}"
+}
