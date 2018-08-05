1e4dbdf8f42507d6f8df5c4a366c1ae250616a30
Michael Musenbrock <michael.musenbrock@gmail.com>
2018-08-05 14:42:33 +0200

    JENKINS-227: uploadFileToWeb: Fail build if upload fails

--- vars/uploadFileToWeb.groovy
+++ vars/uploadFileToWeb.groovy
@@ -4,5 +4,12 @@ def call(def localfile, def upload)
 {
 	echo "Uploading file '${localfile}' to WEB"
 	// +x, otherwise we would spoil the upload token
-	sh "set +x; curl -X POST -k -F upload=@${localfile} '${upload}'"
+	def httpStatus = sh script: "set +x; curl --write-out %{http_code} --silent --output /dev/stderr -X POST -k -F upload=@${localfile} '${upload}'", returnStdout: true
+	// HTTP status code for HTTP OK is 200
+	if (httpStatus == "200") {
+		echo "Upload successful: curl reported HTTP status code: ${httpStatus}"
+	} else {
+		echo "Upload failed: curl reported HTTP status code: ${httpStatus}"
+		currentBuild.result = 'FAILURE'
+	}
 }
